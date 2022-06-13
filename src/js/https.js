075c248fb7664cd2a7317642dfe7ed5a9f6dbf48
arturiamumu <arturiamumu@gmail.com>
2022-06-13 14:06:05 +0800

    share

--- src/js/https.js
+++ src/js/https.js
@@ -1,5 +1,5 @@
-const requestUrl = "http://localhost:9000"
-// const requestUrl = "http://adastra.isamumu.cn:9000"
+// const requestUrl = "http://localhost:9000"
+const requestUrl = "http://adastra.isamumu.cn:9000"
 // const requestUrl = "/app"
 const phone_re = /^1[3|4|5|6|7|8|9][0-9]{9}$/
 const mail_re = /^([a-zA-Z]|[0-9])(\w|\-)+@[a-zA-Z0-9]+\.([a-zA-Z]{2,4})$/
--- src/views/UserInfo.vue
+++ src/views/UserInfo.vue
@@ -1,80 +1,22 @@
 <template>
   <div>
-    <div id="popover-root">
-      <div id="qq" class="share-sub inl" @click="share('qq')">
-        <div class="share-pic">
-          <img src="../assets/qq.png">
-        </div>
-        <div>
-          <el-link :underline="false" type="primary">QQ空间</el-link>
-        </div>
-      </div>
-      <div id="weibo" class="share-sub inl" @click="share('weibo')">
-        <div class="share-pic">
-          <img src="../assets/weibo.png">
-        </div>
-        <div>
-          <el-link :underline="false" type="primary">微博</el-link>
-        </div>
-      </div>
-      <div id="facebook" class="share-sub inl" @click="share('facebook')">
-        <div class="share-pic">
-          <img src="../assets/facebook.png">
-        </div>
-        <div>
-          <el-link :underline="false" type="primary">facebook</el-link>
-        </div>
-      </div>
-      <div id="twitter" class="share-sub inl" @click="share('twitter')">
-        <div class="share-pic">
-          <img src="../assets/twitter.png">
-        </div>
-        <div>
-          <el-link :underline="false" type="primary">twitter</el-link>
-        </div>
-      </div>
-      <div id="linkedin" class="share-sub inl" @click="share('linkedin')">
-        <div class="share-pic">
-          <img src="../assets/linkedin.png">
-        </div>
-        <div>
-          <el-link :underline="false" type="primary">linkedin</el-link>
-        </div>
-      </div>
-      <div id="pinterest" class="share-sub inl" @click="share('pinterest')">
-        <div class="share-pic">
-          <img src="../assets/pinterest.png">
-        </div>
-        <div>
-          <el-link :underline="false" type="primary">pinterest</el-link>
-        </div>
-      </div>
-    </div>
+    <el-button @click="exit">退出登录</el-button>
   </div>
 </template>
 
 <script>
+import {logout} from "@/js/https";
 
 export default {
   name: "UserInfo",
+  methods:{
+    exit() {
+      logout(this)
+    }
+  }
 }
 </script>
 
 <style scoped>
-#popover-root {
-  width: 400px;
-  display: flex;
-  justify-content: space-between;
-}
 
-.share-pic{
-  margin: 0 auto;
-  width: 40px;
-  height: 40px;
-}
-img{
-  width: 100%;
-  height: 100%;
-  border-radius: 20px;
-}
 </style>
