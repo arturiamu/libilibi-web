5570f5032b015720076e5ac96f6713a02045f220
arturiamumu <arturiamumu@gmail.com>
2022-06-24 20:26:18 +0800

    search

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -3,12 +3,18 @@
     <div id="tip">
       <el-link :underline="false" id="res">综合排序:</el-link>
     </div>
-    <div v-for="video in videos" class="inl">
-      <div class="img-div">
-        <img :src="video.pic" @click="play(video)">
+    <div v-for="video in videos" class="inl video-it-sh">
+      <img :src="video.pic" @click="play(video)">
+      <div class="v-count-sh">
+        <el-link class="info" :underline="false" icon="el-icon-video-pause">
+          {{ video.view }}
+        </el-link>
+        <el-link class="info" :underline="false" icon="el-icon-circle-check">
+          {{ video.like }}
+        </el-link>
       </div>
-      <div class="info-div">
-        <el-link :underline="false" v-html="video.title"></el-link>
+      <div class="desc">
+        <el-link :underline="false">{{ video.title }}</el-link>
       </div>
     </div>
   </div>
@@ -29,15 +35,15 @@ export default {
     play: function (video) {
       play_video(this, video)
     },
-    search(keyword,offset,ps){
+    search(keyword, offset, ps) {
       let url = "/video/search/" + keyword + '/' + offset + '/' + ps
-      httpGet(url).then(resp=>{
+      httpGet(url).then(resp => {
         this.videos = resp
       })
     }
   },
   beforeMount() {
-    this.search(this.keyword,0,50)
+    this.search(this.keyword, 0, 50)
   }
 }
 </script>
@@ -48,10 +54,40 @@ export default {
   margin-top: 10px;
 }
 
-.img-div {
-  width: 270px;
-  height: 150px;
-  padding: 10px 5px;
+.video-it-sh {
+  width: 264px;
+  height: 156px;
+  padding-left: 7px;
+  padding-top: 20px;
+  position: relative;
+}
+
+.desc {
+  max-height: 20px;
+  overflow: hidden;
+}
+
+.v-count-sh {
+  text-align: left;
+  position: absolute;
+  top: 154px;
+  width: 97.5%;
+  background-image: linear-gradient(0deg, rgba(160, 163, 165, 0.84) 20%, rgba(246, 246, 246, 0.2) 100%);
+  border-bottom-left-radius: 5px;
+  border-bottom-right-radius: 5px;
+}
+
+.v-count-sh a {
+  margin-left: 10px;
+  color: #ffffff;
+}
+
+.v-count-sh a span {
+  color: #ffffff;
+}
+
+.v-count-sh a i {
+  color: #f6f6f6;
 }
 
 #tip {
@@ -65,12 +101,6 @@ export default {
   font-size: 20px;
 }
 
-.info-div {
-  width: 250px;
-  height: 20px;
-  overflow: hidden;
-  text-align: left;
-}
 
 img {
   width: 100%;
