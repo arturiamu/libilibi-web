36f43bc6d735373324d71ed7cc1a10fad1ba7844
Xvolcan <2416588853@qq.com>
2022-06-24 15:06:36 +0800

    个人中心页面添加三个购买VIP按钮

--- src/views/UserInfo.vue
+++ src/views/UserInfo.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <a href="https://openapi.alipaydev.com/gateway.do">测试支付宝网关地址</a>
-    <el-button @click="getVip1">购买月费会员</el-button>
+    <a href="http://localhost:8080/alipay/pay?subject=monthVip&traceNo=31313131231231231&totalAmount=28"><el-button @click="getVip1">购买月费会员</el-button></a>
     <el-button @click="getVip2">购买半年会员</el-button>
     <el-button @click="getVip3">购买年费会员</el-button>
     <el-button @click="exit">退出登录</el-button>
@@ -20,13 +20,19 @@ export default {
       this.$router.push('/')
     },
     getVip1(){
-      httpGet("/UserVip/getVip?vipLevel=1")
+      let orderNumber = Math.random().toString().substr(3,8)
+      console.log(orderNumber)
+      httpGet("http://localhost:8080/alipay/pay?subject=monthVip&traceNo="+orderNumber+"&totalAmount=28")
     },
     getVip2(){
-      httpGet("/UserVip/getVip?vipLevel=2")
+      let orderNumber = Math.random().toString().substr(3,8)
+      console.log(orderNumber)
+      httpGet("http://localhost:8080/alipay/pay?subject=halfYearVip&traceNo="+orderNumber+"&totalAmount=150")
     },
     getVip3(){
-      httpGet("/UserVip/getVip?vipLevel=3")
+      let orderNumber = Math.random().toString().substr(3,8)
+      console.log(orderNumber)
+      httpGet("http://localhost:8080/alipay/pay?subject=yearVip&traceNo="+orderNumber+"&totalAmount=250")
     },
   }
 }
