2a1252d92af8521734a718a1971eaf657e64d60b
Xvolcan <2416588853@qq.com>
2022-06-24 10:22:38 +0800

    个人中心页面添加三个购买VIP按钮

--- src/views/UserInfo.vue
+++ src/views/UserInfo.vue
@@ -1,5 +1,9 @@
 <template>
   <div>
+    <a href="https://openapi.alipaydev.com/gateway.do">测试支付宝网关地址</a>
+    <el-button @click="getVip1">购买月费会员</el-button>
+    <el-button @click="getVip2">购买半年会员</el-button>
+    <el-button @click="getVip3">购买年费会员</el-button>
     <el-button @click="exit">退出登录</el-button>
   </div>
 </template>
@@ -14,7 +18,16 @@ export default {
       httpGet("/user/logout")
       this.$store.dispatch("clear_user", {})
       this.$router.push('/')
-    }
+    },
+    getVip1(){
+      httpGet("/UserVip/getVip?vipLevel=1")
+    },
+    getVip2(){
+      httpGet("/UserVip/getVip?vipLevel=2")
+    },
+    getVip3(){
+      httpGet("/UserVip/getVip?vipLevel=3")
+    },
   }
 }
 </script>
