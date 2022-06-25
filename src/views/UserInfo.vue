29008c34a4b132e9f32863cc127078a5be340336
arturiamumu <arturiamumu@gmail.com>
2022-06-25 13:50:37 +0800

    update base

--- src/views/UserInfo.vue
+++ src/views/UserInfo.vue
@@ -1,15 +1,27 @@
 <template>
   <div>
     <el-button @click="exit">退出登录</el-button>
+    <el-button @click="modify">修改信息</el-button>
   </div>
 </template>
 
 <script>
-import {httpGet} from "@/js/https";
+import {httpGet, httpPost} from "@/js/https";
 
 export default {
   name: "UserInfo",
-  methods:{
+  methods: {
+    modify() {
+      httpPost('/user/update', {
+        username: 'artmu',
+        items: this.$store.state.user.items
+      }).then(resp => {
+        if (resp.state === 200) {
+          this.$store.dispatch("ch_user", resp.data)
+        }
+        console.log(resp)
+      })
+    },
     exit() {
       httpGet("/user/logout")
       this.$store.dispatch("clear_user", {})
