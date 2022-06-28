bdeae5ae6642381946c73943a1899cf63d27dcd3
Xvolcan <2416588853@qq.com>
2022-06-28 09:37:02 +0800

    个人中心页面添加三个购买VIP按钮

---
+++ src/views/Vip.vue
@@ -0,0 +1,88 @@
+<template>
+  <div>
+    <div>
+      <el-button @click="getVip1">购买月费会员</el-button>
+      <el-button @click="getVip2">购买半年会员</el-button>
+      <el-button @click="getVip3">购买年费会员</el-button>
+      <el-button @click="exit">退出登录</el-button>
+      <el-button >dawdadadadawdawdawdawdadadawdwad</el-button>
+    </div>
+    <div id="pay" v-html="apply">
+
+    </div>
+  </div>
+</template>
+
+<script>
+import {logout, httpGet, httpPost} from "@/js/https";
+
+export default {
+  name: "UserInfo",
+  data() {
+    return {
+      content: '',
+      apply: ''
+    }
+  },
+  methods: {
+    exit() {
+      httpGet("/user/logout")
+      this.$store.dispatch("clear_user", {})
+      this.$router.push('/')
+    },
+    getVip1() {
+      let subject = "mouthvip";
+      console.log("----------------")
+      let money = 25;
+      httpGet("/pay/topay?subject=" + subject + "&money=" + money + "").then(resp => {
+        console.log(resp)
+        // window.open(resp, "adastra 视频分享", 'height=720, width=900, top=100, left=100, toolbar=no, menubar=no, scrollbars=no, resizable=no, location=no, status=no')
+        // this.apply = resp
+        // document.forms[0].submit()
+
+        let divbody=document.createElement('div')
+        divbody.innerHTML=resp   //接口返回的data是一个页面，这里将其添加进新的div
+        document.body.appendChild(divbody)
+        document.forms[0].submit()   //提交表单
+      })
+    },
+
+    getVip2() {
+      let subject = "halfYearVip";
+      let money = 140;
+      httpGet("/pay/topay?subject=" + subject + "&money=" + money + "").then(resp => {
+        console.log(resp)
+        // window.open(resp, "adastra 视频分享", 'height=720, width=900, top=100, left=100, toolbar=no, menubar=no, scrollbars=no, resizable=no, location=no, status=no')
+        // this.apply = resp
+        // document.forms[0].submit()
+
+        let divbody=document.createElement('div')
+        divbody.innerHTML=resp   //接口返回的data是一个页面，这里将其添加进新的div
+        document.body.appendChild(divbody)
+        document.forms[0].submit()   //提交表单
+      })
+    },
+
+    getVip3() {
+      let subject = "yearVip";
+      let money = 260;
+      httpGet("/pay/topay?subject=" + subject + "&money=" + money + "").then(resp => {
+        console.log(resp)
+        // window.open(resp, "adastra 视频分享", 'height=720, width=900, top=100, left=100, toolbar=no, menubar=no, scrollbars=no, resizable=no, location=no, status=no')
+        // this.apply = resp
+        // document.forms[0].submit()
+
+
+        let divbody=document.createElement('div')
+        divbody.innerHTML=resp   //接口返回的data是一个页面，这里将其添加进新的div
+        document.body.appendChild(divbody)
+        document.forms[0].submit()   //提交表单
+      })
+    },
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
